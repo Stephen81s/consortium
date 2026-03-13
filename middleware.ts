c3a26d3956ff2c9560f8e4dcb477a823a0afc3b6
Stephen81s <soupart.stephen@gmail.com>
2026-03-13 18:32:48 +0100

    update

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,7 @@
-// middleware.ts
-import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
 import { NextResponse } from "next/server";
 
-export async function middleware(req) {
-  const res = NextResponse.next();
-  const supabase = createMiddlewareClient({ req, res });
-
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-
-  const isLoginPage = req.nextUrl.pathname.startsWith("/login");
-
-  if (!user && !isLoginPage) {
-    return NextResponse.redirect(new URL("/login", req.url));
-  }
-
-  return res;
+export function middleware() {
+  return NextResponse.next();
 }
 
 export const config = {
